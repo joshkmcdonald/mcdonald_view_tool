cba943f9f03fb6ab7a85e95dc37b0802bbc3971c
joshkmcdonald <joshkmcdonald@gmail.com>
2017-01-23 11:42:13 -0700

    Dev tool initial commit

---
+++ lib/mcdonald_view_tool.rb
@@ -0,0 +1,6 @@
+require "mcdonald_view_tool/version"
+require "mcdonald_view_tool/renderer"
+
+module McdonaldViewTool
+  # Your code goes here...
+end
---
+++ lib/mcdonald_view_tool/version.rb
@@ -0,0 +1,3 @@
+module McdonaldViewTool
+  VERSION = "0.1.0"
+end
